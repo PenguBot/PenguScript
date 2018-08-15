ad913b6abcbbc4daf39f441030a9eba054f8a922
Soumil07 <soumil.paranjpay@gmail.com>
2018-08-15 22:49:19 +0530

    fix index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const Parser = require('./lib/Parser');
 
-module.exports = async (input, opts = {}) => {
+module.exports = async (input, data = {}, opts = {}) => {
 	const parser = new Parser(input, opts);
-	if (!await parser.check())
+	if (!await parser.check()) throw 'Invalid input string.';
+	return parser.parse(data);
 };
